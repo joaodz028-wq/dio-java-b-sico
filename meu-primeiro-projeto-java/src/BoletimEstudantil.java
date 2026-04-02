133d9e1e6d7edab0816097aa1dd0ea6994744264
João Dias <joaodz028@gmail.com>
2026-04-02 13:25:25 -0300

    Add files via upload

---
+++ meu-primeiro-projeto-java/src/BoletimEstudantil.java
@@ -0,0 +1,17 @@
+package java.basico.primeiromes;
+public class BoletimEstudantil {
+
+    public static void main(String[]aStrings){
+
+        int MediaProva=7;
+        if (MediaProva<6) 
+         System.out.println("REPROVADO!");  
+        else if(MediaProva==6)
+            System.out.println("PROVA MINERVA!"); 
+        else
+            System.out.println("APROVADO!");
+            
+    
+
+}
+}
