b8168c14ebe58b629b9abc8ff8ac60538884aaeb
João Dias <Joo Dias>
2026-04-05 13:08:04 -0300

    meu projeto conta banco

---
+++ meu-primeiro-projeto-java/src/curso/basico/primeiromes/ContaTerminal.java
@@ -0,0 +1,15 @@
+package curso.basico.primeiromes;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[]arStrings){
+
+        Scanner scanner=new Scanner(System.in).useLocale(Locale.US);
+
+        
+
+    }
+    
+}
