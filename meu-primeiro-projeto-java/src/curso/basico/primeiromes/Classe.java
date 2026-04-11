c959120b8615a795f5b80acee8e62270d50ad2c8
João Dias <Joo Dias>
2026-04-11 14:45:35 -0300

    alteração usando try e catch

--- meu-primeiro-projeto-java/src/curso/basico/primeiromes/Classe.java
+++ meu-primeiro-projeto-java/src/curso/basico/primeiromes/Classe.java
@@ -1,28 +1,39 @@
 package curso.basico.primeiromes;
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Classe {
     public static void main(String[]arasStrings){
+        try{
       
 
         Scanner scanner= new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome");
+        System.out.println("Digite seu nome:");
         String nome=scanner.next();
 
-        System.out.println("Digite sua idade");
+        System.out.println("Digite seu sobrenome:");
+        String sobrenome=scanner.next();
+
+        System.out.println("Digite sua idade:");
         int idade=scanner.nextInt();
 
-        System.out.println("Digite sua altura");
+        System.out.println("Digite sua altura:");
         double altura=scanner.nextDouble();
 
         
 
-        System.out.println("Olá me chamo " + nome);
+        System.out.println("Olá me chamo " + nome.toUpperCase() +" " +sobrenome.toUpperCase());
         System.out.println("Tenho " + idade +" Anos ");
         System.out.println("Minha altura é " + altura +"cm " );
+        scanner.close();
     }
+    catch(InputMismatchException e){
+        System.err.println("Os campos idade e altura precisam ser númericos");
+    }
+
+   }
 
     
 }
